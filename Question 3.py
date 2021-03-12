00433896453418e91835b00d9f071f006e126faa
varunsai99 <varunmessi9@gmail.com>
2021-03-12 17:19:46 +0530

    final

--- Question 3.py
+++ Question 3.py
@@ -7,6 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1kMZexQXxDTk8iUYHMe4MKyJ0nI9D9S2v
 """
 
+
 from keras.datasets import fashion_mnist
 from matplotlib import pyplot as plt
 import numpy as np
